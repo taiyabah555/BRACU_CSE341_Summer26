fa710b2f4bece0c1acc04888664d0b994bfadf39
taiyabah555 <taiyabah321@gmail.com>
2026-09-21 15:14:06 +0600

    Add files via upload

---
+++ template.asm
@@ -0,0 +1,30 @@
+.MODEL SMALL
+ 
+.STACK 100H
+
+.DATA
+
+; declare variables here
+
+.CODE
+MAIN PROC
+
+; initialize DS
+
+MOV AX,@DATA
+MOV DS,AX
+ 
+; enter your code here
+
+
+
+
+ 
+
+;exit to DOS
+               
+MOV AX,4C00H
+INT 21H
+
+MAIN ENDP
+    END MAIN
